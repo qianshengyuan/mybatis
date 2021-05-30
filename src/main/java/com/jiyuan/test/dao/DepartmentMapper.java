13f3d842b5a20d289efb91c3f95aed361f03ca67
jiyuan <1162765575@qq.com>
2021-05-30 17:08:16 +0800

    Initial commit

---
+++ src/main/java/com/jiyuan/test/dao/DepartmentMapper.java
@@ -0,0 +1,11 @@
+package com.jiyuan.test.dao;
+
+import com.jiyuan.test.bean.Department;
+
+import java.util.List;
+
+public interface DepartmentMapper {
+
+    public List<Department> findAll();
+
+}
